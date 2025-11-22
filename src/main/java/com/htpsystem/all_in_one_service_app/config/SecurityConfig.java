78d434e68a744ca8cdede01cd1b169b4fef91f5d
VEENA SURYAPRAKASH RAO <53285960+Veena55@users.noreply.github.com>
2025-11-22 11:01:12 +0530

    Register (#16)
    * JWT Token,Email verification-token generation
    
    * JWT Token,Email verification-token generation
    
    * Email Sending, Refresh Token
    
    * "implemented-refresh-token"
    
    * Frontend-Backend-Link
    
    * CSRF Token enable
    
    * CSRF Token enable
    
    * CORS
    
    * CORS
    
    * CORS
    
    ---------
    
    Co-authored-by: WebDevVeena <webpronest@gmail.com>

--- src/main/java/com/htpsystem/all_in_one_service_app/config/SecurityConfig.java
+++ src/main/java/com/htpsystem/all_in_one_service_app/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.htpsystem.all_in_one_service_app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -37,8 +38,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        // For development, you can allow localhost:3000. Replace with your frontend URL for production.
-        cfg.setAllowedOrigins(List.of( "http://localhost:*", "http://127.0.0.1:*"));
+        cfg.setAllowedOrigins(List.of(
+                "http://localhost:8081",
+                "http://127.0.0.1:8081"
+        ));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
